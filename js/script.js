deb809f5c3eade436db32271cd9a73624dd08da2
Doniyor <109288985+Doniyor2555@users.noreply.github.com>
2022-12-22 07:47:38 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import cards from './modules/cards';
 import calc from './modules/calc';
 import forms from './modules/forms';
 import slider from './modules/slider';
-import {openModal} from './modules/modal';
+import {openModal} from './modules/modal'
 
 window.addEventListener('DOMContentLoaded', function() {
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
